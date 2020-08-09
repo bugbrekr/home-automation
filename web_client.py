a22e350b9160fe517f6a9bdb991d6e4f4011f650
Chaitanya <chandana.suraneni@gmail.com>
2020-08-09 15:49:22 +0530

    INIT

--- web_client.py
+++ web_client.py
@@ -90,5 +90,29 @@ def test():
 def main():
     app.run(port='80', host='0.0.0.0')
 
+def runner():
+    ft = True
+    while 1:
+        if ft:
+            print('Runner INITIALIZED')
+            ft = False
+        dt = datetime.datetime.now()
+        hour = dt.hour
+        if hour > 8 and hour < 17: #During day
+            print('Tuning off lights periodically')
+            req = requests.get('http://192.168.0.83/2/on')
+            req = requests.get('http://192.168.0.83/3/on')
+            req = requests.get('http://192.168.0.83/4/on')
+            time.sleep(120)
+        elif hour > 11 and hour < 4: #During night
+            print('Tuning off lights periodically')
+            req = requests.get('http://192.168.0.83/2/on')
+            req = requests.get('http://192.168.0.83/3/on')
+            req = requests.get('http://192.168.0.83/4/on')
+            time.sleep(120)
+
 if __name__=='__main__':
-    main()
+    server = threading.Thread(target=main)
+    server.start()
+    runner = threading.Thread(target=runner)
+    runner.start()
