604895b855e6cd1c01edb4cffbafe6f7c4ac40f0
Chaitanya <chandana.suraneni@gmail.com>
2020-08-09 14:22:23 +0530

    INIT

--- runner.py
+++ runner.py
@@ -8,14 +8,14 @@ while 1:
     dt = datetime.datetime.now()
     hour = dt.hour
     if hour > 8 and hour < 17: #During day
-        time.sleep(120)
         print('Tuning off lights periodically')
         req = requests.get('http://192.168.0.83/2/on')
         req = requests.get('http://192.168.0.83/3/on')
         req = requests.get('http://192.168.0.83/4/on')
+        time.sleep(120)
     elif hour > 11 and hour < 4: #During night
-        time.sleep(120)
         print('Tuning off lights periodically')
         req = requests.get('http://192.168.0.83/2/on')
         req = requests.get('http://192.168.0.83/3/on')
         req = requests.get('http://192.168.0.83/4/on')
+        time.sleep(120)
